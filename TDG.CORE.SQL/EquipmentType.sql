5687143c599dcce14d0808fb264f801d0137f789
Ainsley Bernard <ains_bernard@hotmail.com>
2021-06-03 18:03:01 -0400

    update EquipmentType.sql

--- TDG.CORE.SQL/EquipmentType.sql
+++ TDG.CORE.SQL/EquipmentType.sql
@@ -1,5 +1,29 @@
+	--=============================================DYNAMIC VALUES===========================================
+	--these variables can change with the environment, so double check these match the environment you're syncing to
+	--ROM-ACC-TDG-DATA VALUES
+	DECLARE @CONST_TDGCORE_USERID                        VARCHAR(50) = 'ae39bb8b-4b92-eb11-b1ac-000d3ae85ba1';
+	DECLARE @CONST_TDGCORE_DOMAINNAME                    VARCHAR(50) = 'tdg.core@034gc.onmicrosoft.com';
+	DECLARE @CONST_TDG_TEAMID                            VARCHAR(50) = 'd5ddb27a-56b7-eb11-8236-000d3a84ec03';
+	DECLARE @CONST_TDG_TEAMNAME                          VARCHAR(50) = 'Transportation of Dangerous Goods';
+	DECLARE @CONST_TDG_BUSINESSUNITID                    VARCHAR(50) = '4E122E0C-73F3-EA11-A815-000D3AF3AC0D';
+	DECLARE @CONST_PRICELISTID                           VARCHAR(50) = 'b92b6a16-7cf7-ea11-a815-000d3af3a7a7';
 
+	--DEV
+	-- DECLARE @CONST_TDGCORE_BOOKABLE_RESOURCE_ID       VARCHAR(50) = '2cfc9150-d6a3-eb11-b1ac-000d3ae8bee7';
+	-- DECLARE @CONST_TDGCORE_USERID                     VARCHAR(50) = '15abdd9e-8edd-ea11-a814-000d3af3afe0';
+	-- DECLARE @CONST_TDGCORE_DOMAINNAME                 VARCHAR(50) = 'tdg.core@034gc.onmicrosoft.com';
+	-- DECLARE @CONST_TDG_TEAMID                         VARCHAR(50) = 'ed81d4e5-55b7-eb11-8236-0022483bc30f';
+	-- DECLARE @CONST_TDG_TEAMNAME                       VARCHAR(50) = 'Transportation of Dangerous Goods';
+	-- DECLARE @CONST_TDG_BUSINESSUNITID                 VARCHAR(50) = '4E122E0C-73F3-EA11-A815-000D3AF3AC0D';
+	--===================================================================================================
 
+	--===================================================================================================
+	--CRM CONSTANTS
+	DECLARE @CONST_OWNERIDTYPE_TEAM VARCHAR(50) = 'team';
+	DECLARE @CONST_OWNERIDTYPE_SYSTEMUSER VARCHAR(50) = 'systemuser';
+	--===================================================================================================
+
+	select * from STAGING__EQUIPMENT_TYPE
 
 IF EXISTS (
 	SELECT
@@ -12,6 +36,10 @@ IF EXISTS (
 ) DROP TABLE [dbo].[STAGING__EQUIPMENT_TYPE];
 
 CREATE TABLE dbo.STAGING__EQUIPMENT_TYPE (
+    [id] UNIQUEIDENTIFIER NULL,
+    [ovs_name] VARCHAR(100) NULL,
+    [ovs_ohsequipmenttypeen] VARCHAR(100) NULL,
+    [ovs_ohsequipmenttypefr] VARCHAR(250) NULL,
     [createdby] VARCHAR(50) NULL,
     [createdbyname] VARCHAR(100) NULL,
     [createdbyyominame] VARCHAR(100) NULL,
@@ -28,10 +56,6 @@ CREATE TABLE dbo.STAGING__EQUIPMENT_TYPE (
     [modifiedonbehalfbyname] VARCHAR(100) NULL,
     [modifiedonbehalfbyyominame] VARCHAR(100) NULL,
     [overriddencreatedon] DATETIME NULL,
-    [ovs_name] VARCHAR(100) NULL,
-    [ovs_ohsequipmenttypeen] VARCHAR(100) NULL,
-    [ovs_ohsequipmenttypefr] VARCHAR(250) NULL,
-    [id] UNIQUEIDENTIFIER NULL,
     [ownerid] VARCHAR(50) NULL,
     [owneridname] VARCHAR(100) NULL,
     [owneridtype] VARCHAR(50) NULL,
@@ -51,7 +75,7 @@ CREATE TABLE dbo.STAGING__EQUIPMENT_TYPE (
 
 
 DROP TABLE IF EXISTS #STAGING__EQUIPMENT_TYPE;
-GO
+
 
 SELECT
 	CAST(id AS uniqueidentifier) id,
@@ -84,30 +108,7 @@ SELECT '987fa4d2-7499-eb11-b1ac-000d3ae87159', 'Tick removal device or kit', 'Ti
 SELECT 'a27fa4d2-7499-eb11-b1ac-000d3ae87159', 'Winter cleats', 'Winter cleats', 'Crampons à chaussures pour l’hiver' UNION
 SELECT '887fa4d2-7499-eb11-b1ac-000d3ae87159', 'Work gloves', 'Work gloves', 'Gants de travail'
 ) T1;
-	--=============================================DYNAMIC VALUES===========================================
-	--these variables can change with the environment, so double check these match the environment you're syncing to
-	--ROM-ACC-TDG-DATA VALUES
-	DECLARE @CONST_TDGCORE_USERID                        VARCHAR(50) = 'ae39bb8b-4b92-eb11-b1ac-000d3ae85ba1';
-	DECLARE @CONST_TDGCORE_DOMAINNAME                    VARCHAR(50) = 'tdg.core@034gc.onmicrosoft.com';
-	DECLARE @CONST_TDG_TEAMID                            VARCHAR(50) = 'd5ddb27a-56b7-eb11-8236-000d3a84ec03';
-	DECLARE @CONST_TDG_TEAMNAME                          VARCHAR(50) = 'Transportation of Dangerous Goods';
-	DECLARE @CONST_TDG_BUSINESSUNITID                    VARCHAR(50) = '4E122E0C-73F3-EA11-A815-000D3AF3AC0D';
-	DECLARE @CONST_PRICELISTID                           VARCHAR(50) = 'b92b6a16-7cf7-ea11-a815-000d3af3a7a7';
 
-	--DEV
-	-- DECLARE @CONST_TDGCORE_BOOKABLE_RESOURCE_ID       VARCHAR(50) = '2cfc9150-d6a3-eb11-b1ac-000d3ae8bee7';
-	-- DECLARE @CONST_TDGCORE_USERID                     VARCHAR(50) = '15abdd9e-8edd-ea11-a814-000d3af3afe0';
-	-- DECLARE @CONST_TDGCORE_DOMAINNAME                 VARCHAR(50) = 'tdg.core@034gc.onmicrosoft.com';
-	-- DECLARE @CONST_TDG_TEAMID                         VARCHAR(50) = 'ed81d4e5-55b7-eb11-8236-0022483bc30f';
-	-- DECLARE @CONST_TDG_TEAMNAME                       VARCHAR(50) = 'Transportation of Dangerous Goods';
-	-- DECLARE @CONST_TDG_BUSINESSUNITID                 VARCHAR(50) = '4E122E0C-73F3-EA11-A815-000D3AF3AC0D';
-	--===================================================================================================
-
-	--===================================================================================================
-	--CRM CONSTANTS
-	DECLARE @CONST_OWNERIDTYPE_TEAM VARCHAR(50) = 'team';
-	DECLARE @CONST_OWNERIDTYPE_SYSTEMUSER VARCHAR(50) = 'systemuser';
-	--===================================================================================================
 
 INSERT INTO
 	STAGING__EQUIPMENT_TYPE (
